ea50d4b9619a8c9c46b3e3c404049bd5fc6656ca
alfoCaiazza <acaiazza2000@gmail.com>
2024-05-25 08:53:25 +0200

    CORS request handler added to server

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    data = {"message": "Hello from Flask!"}
+    data = {"message": "We reached the top!"}
     return jsonify(data)
 
 if __name__ == '__main__':
